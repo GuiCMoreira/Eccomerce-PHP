7ec32d9bca76b19df1f7a8850b10c6e73876e78a
Maria Fernanda Rizzo <126708422+mfcstt@users.noreply.github.com>
2024-05-11 22:17:46 -0300

    update

---
+++ produtos.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="stylesheet/produtos.css" />
+  </head>
+  <body>
+
+    <div class="produtos_detalhes">
+
+      <div class="div">
+        <img class="product-image" src="img/product-image-3.png" />
+      </div>
+
+   
+      <a href="index.php"><img src="stylesheet/assets/voltar.svg" alt=""></a>
+      
+      <div class="frame">
+        <div class="frame-2">
+          <div class="div-2">
+            <div class="div-3">
+              <div class="nome_produto">Alface</div>
+              <div class="estoque-status"><div class="status">Em estoque</div></div>
+            </div>
+          </div>
+
+          <div class="preco">
+            <div class="frame-3">
+              <div class="preco_produto">$48.00</div>
+            </div>
+          </div>
+          <img class="divisor" src="stylesheet/assets/linha.svg" />
+        </div>
+
+        <!-- VENDEDOR  -->
+        <div class="frame-4">
+            <div class="div-3">
+              <div class="vendedor">Vendedor:</div>
+              <div class="grupo">
+                  <div class="nome_vendedor">farmary</div>
+                  <br>
+                  <p class="descricao_produto">
+                    Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nulla nibh diam,
+                    blandit vel consequat nec, ultrices et ipsum. Nulla varius magna a consequat pulvinar.
+                  </p>
+            </div>
+
+  
+        </div>
+        <div class="adicionar_carrinho">
+          <div class="quantidade">
+            <div class="adicionar_produto"><img class="selecionar" src="stylesheet/assets/diminuir.svg" /></div>
+            <div class="quantidade_selecionada">5</div>
+            <div class="adicionar_produto"><img class="selecionar" src="stylesheet/assets/aumentar.svg" /></div>
+          </div>
+          
+          <button class="button">
+            <div class="texto_carrinho">Adicionar ao Carrinho</div>
+            <img class="rectangle" src="stylesheet/assets/sacola.svg" />
+          </button>
+      
+        </div>
+        <div class="div-2">
+          <div class="div-4">
+            <div class="categorias">Categoria:</div>
+            <div class="id_categoria">Vegetais</div>
+          </div>
+          <div class="div-4">
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
