8fdbf0d7593009c2321a445ae83cf15f25b5656d
Maria Fernanda Rizzo <126708422+mfcstt@users.noreply.github.com>
2024-06-13 09:43:25 -0300

    Update carrinho.php

--- pages/carrinho.php
+++ pages/carrinho.php
@@ -17,7 +17,6 @@ session_start();
 <body>
 
   <header>
-    <section class="itens_carrinho">
 
       <div class="Header">
         <div class="Logo">
@@ -37,56 +36,45 @@ session_start();
   </header>
 
   <main>
-    <div class="carrinho-container">
-      <div class="carrinho-header">
-        <div class="carrinho-titulo">Produto</div>
-        <div class="carrinho-titulo">Preço</div>
-        <div class="carrinho-titulo">Subtotal</div>
-        <div class="carrinho-titulo">Quantidade</div>
-        <div class="carrinho-titulo">Ações</div>
-      </div>
-      <div class="carrinho-items">
-        <?php
-        if (isset($_SESSION['carrinho_serializado'])) {
-          $carrinho = unserialize($_SESSION['carrinho_serializado']);
-          $total = 0;
-          foreach ($carrinho as $item) {
-            echo "<div class='carrinho-item'>";
-            echo "<div class='item-image'>";
-            echo "<img src='$item[3]' alt='Product Image'>";
-            echo "</div>";
-            echo "<div class='item-details'>";
-            echo "<div class='item-nome'>$item[1]</div>";
-            echo "<div class='item-preco'>R$$item[2]</div>";
-            $resultado = $item[0] * $item[2];
-            echo "<div class='item-subtotal'>R$$resultado</div>";
-            echo "<div class='item-quantidade'>";
-            echo "<span id='$item[4]'>$item[0]</span>";
-            echo "</div>";
-            // Imagem de exclusão
-            echo "<div class='item-excluir'>";
-            echo "<form action='../script/removerProduto.php' method='GET'>";
-            echo "<input type='hidden' name='produto_id' value='$item[4]'>";
-            echo "<input type='image' src='../stylesheet/assets/remover.svg' alt='Excluir'>";
-            echo "</form>";
-            echo "</div>";
-            echo "</div>";
-            echo "</div>";
-            $total = $total + $resultado;
+    <section class="carrinho-item">
+      
+      <div class="carrinho-container">
+  
+        <div class="carrinho-items">
+          <?php
+          if (isset($_SESSION['carrinho_serializado'])) {
+            $carrinho = unserialize($_SESSION['carrinho_serializado']);
+            $total = 0;
+            foreach ($carrinho as $item) {
+              echo "<div class='carrinho-item'>";
+              echo "<div class='item-image'>";
+              echo "<img src='$item[3]' alt='Product Image'>";
+              echo "</div>";
+              echo "<div class='item-details'>";
+              echo "<div class='item-nome'>$item[1]</div>";
+              echo "<div class='item-preco'>Preço: R$$item[2]</div>";
+              $resultado = $item[0] * $item[2];
+              echo "<div class='item-subtotal'>Subtotal: R$$resultado</div>";
+              echo "<div class='item-quantidade'>";
+              echo "<span id='$item[4]'> Quantidade: $item[0]</span>";
+              echo "</div>";
+              // Imagem de exclusão
+              echo "<div class='item-excluir'>";
+              echo "<form action='../script/removerProduto.php' method='GET'>";
+              echo "<input type='hidden' name='produto_id' value='$item[4]'>";
+              echo "<input type='image' src='../stylesheet/assets/remover.svg' alt='Excluir'>";
+              echo "</form>";
+              echo "</div>";
+              echo "</div>";
+              echo "</div>";
+              $total = $total + $resultado;
+            }
           }
-        }
-        ?>
-      </div>
-      <div class="carrinho-acoes">
-        <a href="../index.php">
-          <button class="return-to-shop">Voltar a Comprar</button>
-        </a>
-      </div>
-    </div>
-    <form action="finalizarCompra.php">
-      </section>
-
-      <section class="total">
+          ?>
+    </section>
+    
+    <section class="total">
+      <form action="finalizarCompra.php">
         <div class="carrinho-container-2">
           <div class="carrinho-titulo">Carrinho Total</div>
           <div class="carrinho-summary">
@@ -100,17 +88,27 @@ session_start();
       </section>
     </form>
   </main>
-
+  <br>
+  <div class="carrinho-acoes">
+    <a href="../index.php">
+      <button class="return-to-shop">Voltar a Comprar</button>
+    </a>
+  </div>
+</div>
+  </div>
+  
   <br>
   <br>
-
+  <br>
+  <br>
+  
   <footer>
     <div class="Footer">
-    </div>
-    <div class="Footer">
-      <div class="Logo">
-        <a href="index.php">
-          <img src="../stylesheet/assets/logo2.svg" alt="Ecobazar Logo">
+      </div>
+      <div class="Footer">
+        <div class="Logo">
+          <a href="index.php">
+            <img src="../stylesheet/assets/logo2.svg" alt="Ecobazar Logo">
         </a>
       </div>
     </div>
